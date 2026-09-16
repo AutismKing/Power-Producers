00c3812466671407b94785d2d9b4431dc46911df
AutismKing <antalyk007@gmail.com>
2026-09-16 10:07:11 +0100

    fixing?

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var power = 0
 var power_Generator_Effect = 1
-var pg = power_Generator_Effect.toFixed(0);
-var p = power.toFixed(0);
 var Gen_Booster_Effect = 2
 var Gen_Booster_Cost = 10
 var Gen_Booster_CostScaling = 10
@@ -27,8 +25,8 @@ function Knockoff_Boost() {
     Knockoff_Boost_Cost*=Knockoff_Boost_CostScaling
 }
 function UI() {
-    document.getElementById("power").textContent = "You currently have " + p + " power.";
-    document.getElementById("powerGeneratorEffect").textContent = "Gives " + pg + " power";
+    document.getElementById("power").textContent = "You currently have " + power.toFixed(0) + " power.";
+    document.getElementById("powerGeneratorEffect").textContent = "Gives " + power_Generator_Effect.toFixed(0) + " power";
     document.getElementById("GenBoosterCost").textContent = "costs " + Gen_Booster_Cost + " power";
     document.getElementById("GenBoosterEffect").textContent = "x " + Gen_Booster_Effect + " effect of power generator";
     document.getElementById("KnockoffBoostCost").textContent = "costs " + Knockoff_Boost_Cost + " power";
