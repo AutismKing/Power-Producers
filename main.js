63aaa21b2437f543f7e4e4b585accf9837730c30
AutismKing <antalyk007@gmail.com>
2026-09-16 02:44:41 +0100

    maybe this time?

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var power = 0
-var p = power.toFixed(1);
 var power_Generator_Effect = 1
-var pg = powerIncrement.toFixed(1);
+var pg = power_Generator_Effect.toFixed(0);
+var p = power.toFixed(0);
 var Gen_Booster_Effect = 2
 var Gen_Booster_Cost = 10
 var Gen_Booster_CostScaling = 10
@@ -17,21 +17,21 @@ function subPower() {
 function Gen_Booster() {
     if (power < Gen_Booster_Cost) return
     power-=Gen_Booster_Cost
-    powerIncrement*=Gen_Booster_Effect
+    power_Generator_Effect*=Gen_Booster_Effect
     Gen_Booster_Cost*=Gen_Booster_CostScaling
 }
 function Knockoff_Boost() {
     if (power < Knockoff_Boost_Cost) return
     power-=Knockoff_Boost_Cost
-    powerIncrement*=Knockoff_Boost_Effect
+    power_Generator_Effect*=Knockoff_Boost_Effect
     Knockoff_Boost_Cost*=Knockoff_Boost_CostScaling
 }
 function UI() {
     document.getElementById("power").textContent = "You currently have" + p + "power.";
     document.getElementById("powerGeneratorEffect").textContent = "Gives " + pg + " power";
     document.getElementById("GenBoosterCost").textContent = "costs " + Gen_Booster_Cost + " power";
-    document.getElementById("GenBoosterEffect").textContent = "x " + Gen_Booster_Effect + "effect of power generator";
+    document.getElementById("GenBoosterEffect").textContent = "x" + Gen_Booster_Effect + " effect of power generator";
     document.getElementById("KnockoffBoostCost").textContent = "costs " + Knockoff_Boost_Cost + " power";
-    document.getElementById("KnockoffBoostEffect").textContent = "x " + Knockoff_Boost_Effect + "effect of power generator";
+    document.getElementById("KnockoffBoostEffect").textContent = "x" + Knockoff_Boost_Effect + " effect of power generator";
 }
 setInterval(UI, 50)
