80b0c54f04652a17cc54443cef16a372477708dc
AutismKing <antalyk007@gmail.com>
2026-09-16 02:34:22 +0100

    masive overhaul

--- main.js
+++ main.js
@@ -1,59 +1,37 @@
-var power = 0
+var power = 1
+var p = power.toFixed(1);
 var powerIncrement = 1
-var U2Cost = 10
-var U2CostScaling = 10
-var U3Cost = 150
-var U4Cost = 500
-var superCost = 1000
-var superPoint = 0
-var superPointEffect = 1
-function format(amount) {
-    return amount.toFixed(0);
+var pi = powerIncrement.toFixed(1);
+var Gen_Booster_Effect = 2
+var Gen_Booster_Cost = 10
+var Gen_Booster_CostScaling = 10
+var Knockoff_Boost_Effect = 1.25
+var Knockoff_Boost_Cost = 25
+var Knockoff_Boost_CostScaling = 5
+function addPower() {
+    power+=powerIncrement
 }
-setInterval(function U1() {
-    power += powerIncrement
-}, 1000);
 function subPower() {
     power--
 }
-function U2() {
-    if (power < U2Cost) return
-    power-=U2Cost
-    powerIncrement+=1
-    U2Cost*=U2CostScaling
+function Gen_Booster() {
+    if (power < Gen_Booster_Cost) return
+    power-=Gen_Booster_Cost
+    powerIncrement*=Gen_Booster_Effect
+    Gen_Booster_Cost*=Gen_Booster_CostScaling
 }
-function U3() {
-    if (power < U3Cost) return
-    power-=U3Cost
-    powerIncrement*=2
-    U3Cost*=10
-}
-function U4() {
-    if (power < U4Cost) return
-    power-=U4Cost
-    U2CostScaling*=0.9
-    U2Cost*=0.9
-    U4Cost*=10
-}
-function superPointUpgrade() {
-    if (power < superCost) return
-    power-=superCost
-    superPoint+=1
-    superPointEffect+=superPoint
-    powerIncrement*=superPointEffect
-    powerIncrement*=2
-    superCost*=10
+function Knockoff_Boost() {
+    if (power < Knockoff_Boost_Cost) return
+    power-=Knockoff_Boost_Cost
+    powerIncrement*=Knockoff_Boost_Effect
+    Knockoff_Boost_Cost*=Knockoff_Boost_CostScaling
 }
 function UI() {
-    document.getElementById("power").textContent = "You are currently producing " + format(power) + " power per second";
-    document.getElementById("U2Cost").textContent = "costs " + U2Cost + " power";
-    document.getElementById("powerIncrement").textContent = "+ " + format(powerIncrement) + " power";
-    document.getElementById("U3Cost").textContent = "costs " + U3Cost + " power";
-    document.getElementById("U4Cost").textContent = "costs " + U4Cost + " power";
-    document.getElementById("superCost").textContent = "costs " + superCost + " power";
-    document.getElementById("superPoint").textContent = "you currently have " + superPoint + " SuperPoints, multiplying all Power production by " + superPointEffect;
+    document.getElementById("power").textContent = "You currently have" + p + "power.";
+    document.getElementById("powerIncrement").textContent = "Gives " + pi + " power";
+    document.getElementById("GenBoosterCost").textContent = "costs " + Gen_Booster_Cost + " power";
+    document.getElementById("GenBoosterEffect").textContent = "x " + Gen_Booster_Effect + "effect of power generator";
+    document.getElementById("KnockoffBoostCost").textContent = "costs " + Knockoff_Boost_Cost + " power";
+    document.getElementById("KnockoffBoostEffect").textContent = "x " + Knockoff_Boost_Effect + "effect of power generator";
 }
 setInterval(UI, 50)
-setInterval(function updateMyGame() {
-    power += powerIncrement
-}, 1000);
